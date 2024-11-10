6159d67f35351560908a7c11c152de1c4e1fdf00
AaryaR <77625871+aaaaryaaa@users.noreply.github.com>
2024-11-10 23:42:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div class="">
-        <h1>Hello, Everyone! <span class="emoji">🌟</span></h1>
-        <p>I'm <span class="highlight">Aarya R</span>, Head of Web Development at ISTE Manipal!</p>
-        <p>I’m here to guide the development of our primary platform, the ISTE website, and to work with all of you on exciting projects that push boundaries. I'm always eager to discuss new project ideas, startups, or anything innovative in tech.</p>
+      <div>
+        <h1>Hello, Everyone! <span className="emoji">🌟</span></h1>
+        <p>I'm <span className="highlight">Aarya R</span>, Head of Web Development at ISTE Manipal!</p>
+        <p>I’m here not only to guide the development of our primary platform, the ISTE website, but also to mentor and support everyone in the web development domain. Whether it's working on exciting projects, exploring new technologies, or sharing insights on best practices, I'm committed to helping you grow as developers. I’m always eager to discuss innovative ideas, startups, and anything that pushes the boundaries of what we can achieve together in tech.</p>
         <p>I'm a 3rd-year CSE student at MIT, so feel free to reach out if you have any general questions or need help with anything CSE-related.</p>
-        <p class="emoji">Looking forward to an incredible journey with all of you! Let’s make some magic happen! 🚀</p>
+        <p className="emoji">Looking forward to an incredible journey with all of you! Let’s make some magic happen! 🚀</p>
       </div>
     </div>
   )
